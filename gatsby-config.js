4c1997283052f6aeb85d95e6e11ee1d42bc5494d
Brian Sunbury <bsunbury29@gmail.com>
2020-10-14 23:18:26 -0700

    WIP 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-netlify-identity`,
+      options: {
+        url: `https://srianbury-gatsby-netlify-example.netlify.app/`, // required!
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
