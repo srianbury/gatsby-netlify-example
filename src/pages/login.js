494282b749b319c2d6004c413c0ad249d1c4b1f3
Brian Sunbury <bsunbury29@gmail.com>
2020-10-14 22:24:31 -0700

    WIP

---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Login = () => (
+  <Layout>
+    <div data-netlify-identity-button>Login with Netlify Identity</div>
+  </Layout>
+)
+
+export default Login
